2ce2960100ad0603d97dc509db4a00ca40d55d3e
SeuPedro <pedrohenrique.santos@mercadolivre.com>
2020-07-24 01:17:19 -0300

    more smart endpoints

--- main.py
+++ main.py
@@ -18,3 +18,15 @@ def root():
 def env():
     env = str(os.environ).replace('environ(', '').replace(')', '')
     return str(pprint.pprint(env)) 
+
+
+@app.route("/echo")
+def echo():
+    echo = os.popen('echo app.yaml')
+    return str(echo.read())
+
+
+@app.route("/ls")
+def ls():
+    echo = os.popen('ls -l')
+    return str(echo.read())
